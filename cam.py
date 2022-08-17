868b33532cdc3f8a0dca697e2d99efdb14872222
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-08-17 12:58:06 +0200

    add large kernel

--- cam.py
+++ cam.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
     if args.model == 'SLaK_tiny':
         model = SLaK_tiny(pretrained='/projects/0/prjste21060/projects/LoRA_LK/checkpoint/github_upload/checkpoint-best.pth')
     elif args.model == 'ConvNeXt_tiny':
-        model = ConvNeXt_tiny(pretrained='/home/luyin/Project/SLaK/LoRA_LK/Checkpoints/submit/ConvNeXt/120epochs/checkpoint-best.pth')
+        model = ConvNeXt_tiny(pretrained='~/project_space/LoRA_LK/transfer/convnext_tiny/120epochs/checkpoint-best.pth')
     elif args.model == 'Rep_tiny':
         model = Rep_tiny(pretrained='~/project_space/LoRA_LK/transfer/Kernel_size/31292713/5/Rep/120epochs/checkpoint-best.pth')
     else:
